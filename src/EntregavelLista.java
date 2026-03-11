5a8a8b48e38d1c401d54ab7e2a8bc405035d2e0a
Pedro Henrique Franceschi <pedrohenriquekyre@gmail.com>
2026-03-11 13:40:39 -0300

    excluí o arquivo main pois não precisava, fiz a base do trabalho

--- src/EntregavelLista.java
+++ src/EntregavelLista.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class EntregavelLista {
+    static void main() {
+
+        Scanner scan = new Scanner(System.in);
+        List<Integer> numeros = new ArrayList<>();
+
+        System.out.println("Informe o tamanho da lista: ");
+        int tamanho = scan.nextInt();
+
+        for (int i = 0; i < tamanho; i++){
+            System.out.println("Digite o valor " + (i + 1) + ": ");
+            int valor = scan.nextInt();
+            numeros.add(valor);
+        }
+        // a) Imprimir apenas os números pares (filter)
+
+        // b) Imprimir os valores dobrados (map)
+
+        // c) Imprimir os valores em ordem crescente (sorted)
+
+        System.out.println("\n--- Lista original com forEach da List ---");
+        for (int numero : numeros){
+            System.out.println(numero);
+        }
+        scan.close();
+
+
+    }
 }
